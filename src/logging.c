6270a69c38a1549fc04c57a745ca0b77ecfd38bc
bbbbbr <bbbbbr@users.noreply.github.com>
2023-07-21 17:18:24 -0700

    Logging: add debug logging level

--- src/logging.c
+++ src/logging.c
@@ -8,43 +8,41 @@
 int output_level = OUTPUT_LEVEL_DEFAULT;
 
 
+#define VA_LIST_PRINT() \
+    va_list args; \
+    va_start (args, format); \
+    vprintf (format, args); \
+    va_end (args); \
+    fflush(stdout);
+
+
 void log_set_level(int new_output_level) {
     output_level = new_output_level;
 }
 
+void log_debug(const char * format, ...){
+
+    if (output_level > OUTPUT_LEVEL_DEBUG) return;
+    VA_LIST_PRINT();
+}
+
 void log_verbose(const char * format, ...){
 
-    va_list args;
-
-    // Only print if vermose mode IS enabled
-    if (output_level != OUTPUT_LEVEL_VERBOSE) return;
-
-    va_start (args, format);
-    vprintf (format, args);
-    va_end (args);
+    if (output_level > OUTPUT_LEVEL_VERBOSE) return;
+    VA_LIST_PRINT();
 }
 
 void log_standard(const char * format, ...){
 
-    va_list args;
-
     // Only print if quiet mode and error_only are NOT enabled
     if ((output_level == OUTPUT_LEVEL_QUIET) ||
         (output_level == OUTPUT_LEVEL_ONLY_ERRORS)) return;
-
-    va_start (args, format);
-    vprintf (format, args);
-    va_end (args);
+    VA_LIST_PRINT();
 }
 
 void log_error(const char * format, ...){
 
-    va_list args;
-
     // Only print if quiet mode is NOT enabled
     if (output_level == OUTPUT_LEVEL_QUIET) return;
-
-    va_start (args, format);
-    vprintf (format, args);
-    va_end (args);
+    VA_LIST_PRINT();
 }
--- src/logging.h
+++ src/logging.h
@@ -4,6 +4,7 @@
 #define _LOGGING_H
 
 enum output_levels {
+    OUTPUT_LEVEL_DEBUG,
     OUTPUT_LEVEL_VERBOSE,
     OUTPUT_LEVEL_DEFAULT,
     OUTPUT_LEVEL_ONLY_ERRORS,
@@ -11,6 +12,7 @@ enum output_levels {
 };
 
 void log_set_level(int new_output_level);
+void log_debug(const char * format, ...);
 void log_verbose(const char * format, ...);
 void log_standard(const char * format, ...);
 void log_error(const char * format, ...);
