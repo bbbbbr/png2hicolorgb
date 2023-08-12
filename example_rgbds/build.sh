74212c0031b1683f0f93b68bdad49e57a266204f
bbbbbr <bbbbbr@users.noreply.github.com>
2023-08-12 00:22:22 -0700

    Example: RGBDS: improve conversion a little by turning on Left/Right adaptive

--- example_rgbds/build.sh
+++ example_rgbds/build.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 mkdir -p bin obj
 # --type=1  : Median cut conversion with no dither
 # --vaddrid : Map uses vram id instead of sequential tile order id
-../bin/png2hicolorgb res/example_image.png --type=1 -o obj/example_image.til --vaddrid
+# -L=1 and -R=1 : Use adaptive mode for CGB palette block sizes (better image fidelity)
+../bin/png2hicolorgb res/example_image.png --type=1 -L=1 -R=1 -o obj/example_image.til --vaddrid
 rgbasm -o obj/hicolor.o hicolor.asm
 rgbasm -o obj/main.o main.asm
 rgblink -m bin/hicolor.map -n bin/hicolor.sym -o bin/hicolor.gbc obj/hicolor.o obj/main.o
